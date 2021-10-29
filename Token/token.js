dbb0ff759cff27d6205eb1de2d6d54c78f77cdbe
Joannekess <71588418+Joannekess@users.noreply.github.com>
2021-10-29 09:49:57 +0700

    blom beres

--- Token/token.js
+++ Token/token.js
@@ -1,9 +1,15 @@
-    function btntest_onclick(){
-        if (document.getElementById('TokenInput').value == '1234') {
-            window.location.href = "http://www.google.com";
-        } else if(document.getElementById('TokenInput').value == '0000'){
-            window.location.href = "http://www.bing.com";
-        } else{
-            alert("Token yang Anda isi salah!");
-        }
-    };
+function btntest_onclick() {
+  if (document.getElementById("TokenInput").value == "1234") {
+    Swal.fire({
+      //   title: "Sweet!",
+      //   text: "Modal with a custom image.",
+      imageUrl: "/images/lolos.png",
+      imageAlt: "Custom image",
+      showConfirmButton: false,
+    });
+  } else if (document.getElementById("TokenInput").value == "0000") {
+    window.location.href = "http://www.bing.com";
+  } else {
+    alert("Token yang Anda isi salah!");
+  }
+}
